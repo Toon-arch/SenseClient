8470db5b9bddd256a370ba28cdca4833212089bf
Toon <85719086+Toon-arch@users.noreply.github.com>
2021-11-05 22:29:15 -0400

    Update universal.lua

--- profiles/universal.lua
+++ profiles/universal.lua
@@ -1,1 +1,3 @@
-loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/Toon-arch/SenseClient/main/main.lua")))().savefile("custom/universal");
+if not isfolder("sense-client") then makefolder("sense-client") end
+if not isfolder("sense-client/custom") then makefolder("sense-client/custom") end
+loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/Toon-arch/SenseClient/main/main.lua")))().savefile("custom/universal")
